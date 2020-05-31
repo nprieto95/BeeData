ccd8c0e7128e3a49e4d03db61ccbc6fa81f3486f
Nahuel Prieto <nprieto@smartplanets.world>
2020-05-31 18:19:01 -0300

    added sample implementation for global temperature

---
+++ BeeData.CovidSpaceAppsDemo/BeeData.CovidSpaceAppsDemo.SampleImplementations/WorldTemperatureAnomalyPreScoreProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace BeeData.CovidSpaceAppsDemo.SampleImplementations
+{
+
+    public class WorldTemperatureAnomalyPreScoreProvider : IPreScoreProvider
+    {
+
+        private readonly IDictionary<int, double> anomalies;
+        readonly double min, range;
+
+        public WorldTemperatureAnomalyPreScoreProvider()
+        {
+            var client = new HttpClient();
+            var text = client.GetAsync("https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt").Result.Content.ReadAsStringAsync().Result;
+            var lines = text.Split('\n');
+            anomalies = new Dictionary<int, double>();
+            for (int i = 5; i < lines.Length; i++)
+            {
+                anomalies.Add(int.Parse(lines[i].Substring(0, 4)), double.Parse(lines[i].Substring(9, 5)));
+            }
+            min = anomalies.Values.Min();
+            var max = anomalies.Values.Max();
+            range = max - min;
+        }
+
+        public double GetPreScore(DateTime dateTime)
+        {
+            return 1 - ((anomalies[dateTime.Year] - min) / range);
+        }
+
+    }
+
+}
